e1d1b25e5462f4359bbf4dbfb9598e92d2588844
Ali1 <ali.m.bakir@gmail.com>
2019-12-26 18:59:48 +0000

    Create ParamConverterMiddleware.php

---
+++ src/ParamConverterMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ParamConverter;
+
+use Cake\Controller\ControllerFactory;
+use Cake\Core\Configure;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class ParamConverterMiddleware implements \Psr\Http\Server\MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $factory = new ControllerFactory();
+        $class = $factory->getControllerClass($request);
+        if (!$class) {
+            // Controller does not exist. Let Cake handle it naturally (MissingControllerException)
+            $class = get_class($factory->create($request, $response));
+        }
+
+        $converters = [];
+        foreach (Configure::readOrFail('ParamConverter.converters') as $converter) {
+            $converters[] = new $converter;
+        }
+        $manager = new ParamConverterManager($converters);
+        $action = $request->getParam('action');
+        $request = $manager->apply($request, $class, $action);
+        return $handler->handle($request);
+    }
+}
